27cb64385bc931529cec3fc4f63e740827ad8ce4
prkm93 <aradshub93@gmail.com>
2021-10-12 13:58:47 +0530

    react testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+// setup file
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+configure({ adapter: new Adapter() });
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+describe("Counter testing", () => {
+  test("render title of counter",  () => {
+    const wrapper = shallow(<App/>);
+    
+  })
+})
